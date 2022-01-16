a7491fcf20ba0a901ad3b6c0355410c9a4474890
rycont <reactdev@kakao.com>
2022-01-16 09:12:29 +0000

    feat: creating processable route to api route

--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,13 +1,33 @@
 import { Vexile } from "@haechi/flexile"
 import { modalContentAtom } from "coil"
 import { Button, Header, Input, XDesc } from "components"
+import { connect } from "connector"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import { useState } from "react"
 import { useRecoilState } from "recoil"
 
 export const NewPlaylist: React.FC = () => {
     const [playlistURI, setPlaylistURI] = useState<string>()
     const setModal = useRecoilState(modalContentAtom)[1]
+    const router = useRouter()
+
+    const processPlaylist = async () => {
+        setModal({
+            title: "잠시만 기다려주세요 ..",
+            content: "플레이리스트 주소를 가져오고 있어요",
+            dismissable: true,
+        })
+
+        const { createdId } = await connect<unknown, { createdId: string }>(
+            "createPlaylist",
+            {
+                playlistURI,
+            }
+        )
+
+        router.push(`/playlist/${createdId}`)
+    }
 
     return (
         <Vexile filly gap={6} y="center" x="center">
@@ -20,28 +40,7 @@ export const NewPlaylist: React.FC = () => {
                 icon="/icons/link.svg"
                 onChange={setPlaylistURI}
             />
-            <Link
-                href={{
-                    pathname: "/fetch-uri",
-                    query: {
-                        playlistURI,
-                    },
-                }}
-            >
-                <a>
-                    <Button
-                        onClick={() =>
-                            setModal({
-                                title: "잠시만 기다려주세요 ..",
-                                content: "플레이리스트 주소를 가져오고 있어요",
-                                dismissable: true,
-                            })
-                        }
-                    >
-                        다음
-                    </Button>
-                </a>
-            </Link>
+            <Button onClick={processPlaylist}>다음</Button>
         </Vexile>
     )
 }
